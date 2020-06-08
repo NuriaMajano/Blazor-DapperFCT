1d53620fa2ae9263637ecc338a2cfeb807142826
Nuria Majano Santiago <alumno.113633@iesazarquiel.es>
2020-06-08 19:41:39 +0200

    commit FilmsDetails

---
+++ BlazorCRUB/BlazorCRUB.Model/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Dynamic;
+
+namespace BlazorCRUB.Model
+{
+    public class Film
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
